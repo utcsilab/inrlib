047c661f1c3d93ee85425db792f6d25740bb4c8d
zstoebs <zstoebner25@gmail.com>
2024-01-16 22:43:03 -0600

    init update

--- inrlib/__init__.py
+++ inrlib/__init__.py
@@ -1,13 +1,13 @@
-from inrlib.data import ABCDataset
-from inrlib.data.DataModules import *
-from inrlib.data.Datasets import *
+from .data import ABCDataset
+from .data.DataModules import *
+from .data.Datasets import *
 
-from inrlib.models import ABCModel
-from inrlib.models.MLP import *
+from .models import ABCModel
+from .models.MLP import *
 
-from inrlib.losses import ABCLoss
-from inrlib.losses.MSE import * 
+from .losses import ABCLoss
+from .losses.MSE import * 
 
-from inrlib.utils import *
-from inrlib.ImageLoggers import *
-from inrlib.Transforms import *
+from .utils import *
+from .ImageLoggers import *
+from .Transforms import *
