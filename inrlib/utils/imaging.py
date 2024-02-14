e0b900d84f7e96e760fef3594522d00cdb82e022
zstoebs <zstoebner25@gmail.com>
2024-02-14 11:01:59 -0600

    robust reshape

--- inrlib/utils/imaging.py
+++ inrlib/utils/imaging.py
@@ -14,7 +14,7 @@ def reshape(arr: Union[np.ndarray, torch.Tensor], shape: Tuple[int, ...]) -> Uni
     elif isinstance(arr, torch.Tensor):
         out = arr.clone()
     
-    return out.reshape(shape) if tuple(out.shape) != shape else out
+    return out.reshape(*shape) if tuple(out.shape) != shape else out
 
 
 # @title NP Area Resize Code
