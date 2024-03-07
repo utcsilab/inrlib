476f1ad953869c2bca6aaa79077cc9c4d3654377
zstoebs <zstoebner25@gmail.com>
2024-03-06 18:33:59 -0600

    debugged state in abcmodel

--- inrlib/models/__init__.py
+++ inrlib/models/__init__.py
@@ -11,6 +11,17 @@ class ABCModel(ABC, pl.LightningModule):
 	def on_validation_start(self):
 		self.outputs = []
 		self.scores = None
+		super().on_validation_start()
+	
+	def on_test_start(self):
+		self.outputs = []
+		self.scores = None
+		super().on_test_start()
+  
+	def on_predict_start(self) -> None:
+		self.outputs = []
+		self.scores = None
+		super().on_predict_start()
 		
 	@abstractmethod
 	def reconstruct(self):
