71077202ab9ba5116d173dacd17a8684d3173ddd
zstoebs <zstoebner25@gmail.com>
2024-03-25 23:16:13 -0500

    removed extran check

--- inrlib/utils/imaging.py
+++ inrlib/utils/imaging.py
@@ -16,7 +16,7 @@ def reshape(arr: Union[np.ndarray, torch.Tensor], shape: Tuple[int, ...]) -> Uni
     elif isinstance(arr, torch.Tensor):
         out = arr.clone()
     
-    return out.reshape(*shape) if tuple(out.shape) != shape else out
+    return out.reshape(*shape)
 
 
 # @title NP Area Resize Code
