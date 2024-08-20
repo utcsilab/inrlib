ab0f154f722d9950f5b67d4b3e14cb92ab583dc0
zstoebs <zstoebner25@gmail.com>
2024-08-20 16:31:19 -0500

    added DWT

--- inrlib/Transforms.py
+++ inrlib/Transforms.py
@@ -1,9 +1,11 @@
 import numpy as np
-from typing import Union, List, Literal
+from typing import Union, List, Literal, Tuple
 import torch
 
+import ptwt, pywt # pytorch wavelets, pywavelets
+
 from .utils.imaging import fft, ifft
-from .utils.numeric import make_complex 
+from .utils.numeric import make_complex, make_real
 
 from abc import ABC, abstractmethod
 
@@ -39,5 +41,45 @@ class PhaseTransform(ABCTransform):
 
 
 class WaveletTransform(ABCTransform):
-    def __call__(self, x: Union[torch.Tensor, np.ndarray]) -> Union[torch.Tensor, np.ndarray]:
-        raise NotImplementedError("WaveletTransform not implemented yet")
+    def __init__(self, wavelet='db1', mode='symmetric', dims: Tuple[int, ...]=(0,1), level: int=1, **kwargs):
+        
+        super().__init__(**kwargs)
+        self.wavelet = wavelet
+        self.mode = mode
+        self.dims = dims
+        self.level = level
+    
+    def __call__(self, x: Union[torch.Tensor, np.ndarray]):
+        compl = make_real(x) # ptwt expects float input
+        coefs = ptwt.wavedec2(compl, self.wavelet, mode=self.mode, axes=self.dims, level=self.level)
+        return coefs
+  
+
+class InverseWaveletTransform(ABCTransform):
+    def __init__(self, wavelet='db1', mode='symmetric', dims: Tuple[int, ...]=(0,1), **kwargs):
+        super().__init__(**kwargs)
+
+        self.wavelet = wavelet
+        self.mode = mode
+        self.dims = dims
+    
+    def __call__(self, coefs: torch.Tensor):
+        return ptwt.waverec2(coefs, self.wavelet, axes=self.dims)
+
+
+if __name__ == '__main__':
+    wavelet_transform = WaveletTransform(wavelet='db1', mode='symmetric', dims=(0, 1), level=4)
+    inverse_wavelet_transform = InverseWaveletTransform(wavelet='db1', mode='symmetric', dims=(0, 1))
+
+    # Input tensor (replace with actual data)
+    x = torch.randn(32, 32, 3, 2)  # Example 2D image with shape (batch_size, channels, height, width)
+
+    # Perform forward wavelet transform
+    coefs = wavelet_transform(x)
+
+    # Perform inverse wavelet transform
+    x_reconstructed = inverse_wavelet_transform(coefs)
+
+    # Check reconstruction error
+    reconstruction_error = torch.norm(x - x_reconstructed)
+    print(f'Reconstruction error: {reconstruction_error.item()}')
