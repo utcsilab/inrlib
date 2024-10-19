5d673a783937d833f166da2c0466f11e344bf0d0
zstoebs <zstoebner25@gmail.com>
2024-10-19 17:52:52 -0500

    handling complex init

--- inrlib/layers/initializers.py
+++ inrlib/layers/initializers.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from typing import List, Callable, Tuple, Mapping, Optional
 
+from .layers import ComplexLinear
 
 class XavierNormal(nn.Module):
     def __init__(self, gain: float = 1.0):
@@ -12,6 +13,10 @@ class XavierNormal(nn.Module):
         if isinstance(m, nn.Linear):
             nn.init.xavier_normal_(m.weight, self.gain)
             nn.init.constant_(m.bias, 0)
+        if isinstance(m, ComplexLinear):
+            nn.init.xavier_normal_(m.weight.real, self.gain)
+            nn.init.xavier_normal_(m.weight.imag, self.gain)
+            nn.init.constant_(m.bias, 0.+1j*0.)
 
 
 class XavierUniform(nn.Module):
@@ -23,6 +28,10 @@ class XavierUniform(nn.Module):
         if isinstance(m, nn.Linear):
             nn.init.xavier_uniform_(m.weight, self.gain)
             nn.init.constant_(m.bias, 0)
+        if isinstance(m, ComplexLinear):
+            nn.init.xavier_uniform_(m.weight.real, self.gain)
+            nn.init.xavier_uniform_(m.weight.imag, self.gain)
+            nn.init.constant_(m.bias, 0.+1j*0.)
 
 
 class KaimingNormal(nn.Module):
@@ -36,6 +45,10 @@ class KaimingNormal(nn.Module):
         if isinstance(m, nn.Linear):
             nn.init.kaiming_normal_(m.weight, self.a, self.mode, self.nonlinearity)
             nn.init.constant_(m.bias, 0)
+        if isinstance(m, ComplexLinear):
+            nn.init.kaiming_normal_(m.weight.real, self.a, self.mode, self.nonlinearity)
+            nn.init.kaiming_normal_(m.weight.imag, self.a, self.mode, self.nonlinearity)
+            nn.init.constant_(m.bias, 0.+1j*0.)
 
 
 class KaimingUniform(nn.Module):
@@ -49,3 +62,7 @@ class KaimingUniform(nn.Module):
         if isinstance(m, nn.Linear):
             nn.init.kaiming_uniform_(m.weight, self.a, self.mode, self.nonlinearity)
             nn.init.constant_(m.bias, 0)
+        if isinstance(m, ComplexLinear):
+            nn.init.kaiming_uniform_(m.weight.real, self.a, self.mode, self.nonlinearity)
+            nn.init.kaiming_uniform_(m.weight.imag, self.a, self.mode, self.nonlinearity)
+            nn.init.constant_(m.bias, 0.+1j*0.)
